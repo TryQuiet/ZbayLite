e4969b174305a5c87699b2a796dd0cfe359a4702
NorbertBodziony <norbert.bodziony@rumblefish.pl>
2020-08-13 13:09:48 +0200

    Zbay lite CI support (#14)
    CI build for lite version

--- src/main/cli/client.js
+++ src/main/cli/client.js
@@ -1,6 +1,15 @@
+// /* eslint-disable */
 import { Worker } from 'worker_threads'
-const worker = new Worker('./src/main/cli/worker.js')
+import path from 'path'
 
+const isDev = process.env.NODE_ENV === 'development'
+const pathDev = path.join.apply(null, [process.cwd(), 'worker/cli.worker.js'])
+const pathProd = path.join.apply(null, [
+  process.resourcesPath,
+  'worker/cli.worker.js'
+])
+
+const worker = new Worker(isDev ? pathDev : pathProd)
 var mapping = new Map()
 export default class Client {
   constructor () {
@@ -17,9 +26,7 @@ export default class Client {
         args: JSON.stringify({ id: id, method: method, args: args })
       })
     })
-    worker.postMessage(
-      JSON.stringify({ id: id, method: method, args: args })
-    )
+    worker.postMessage(JSON.stringify({ id: id, method: method, args: args }))
     return promise
   }
 }
