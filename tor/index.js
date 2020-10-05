35ab568c4a12e03fa416988cbd3af58edc2df37e
Norbert <norbertbodziony@gmail.com>
2020-10-05 14:26:50 +0200

    fix tor hidden_service

--- tor/index.js
+++ tor/index.js
@@ -1,15 +1,39 @@
 const path = require('path')
 const isDev = process.env.NODE_ENV === 'development'
+
 const pathDev = path.join.apply(null, [process.cwd(), 'tor/tor'])
 const pathDevSettings = path.join.apply(null, [process.cwd(), 'tor/torrc'])
 const pathProd = path.join.apply(null, [process.resourcesPath, 'tor/tor'])
 const pathProdSettings = path.join.apply(null, [
   process.resourcesPath,
-  'tor/torrcProd'
+  'tor/torrc'
 ])
+console.log()
+const os = require('os')
+
+console.log(`${os.homedir()}/zbay_tor`)
 const spawn = require('child_process').spawn
 const spawnTor = () =>
   new Promise((resolve, reject) => {
+    var fs = require('fs')
+    fs.readFile(isDev ? pathDevSettings : pathProdSettings, 'utf8', function (
+      err,
+      data
+    ) {
+      if (err) {
+        return console.log(err)
+      }
+      var result = data.replace(/PATH_TO_CHANGE/g, `${os.homedir()}/zbay_tor`)
+
+      fs.writeFile(
+        isDev ? pathDevSettings : pathProdSettings,
+        result,
+        'utf8',
+        function (err) {
+          if (err) return console.log(err)
+        }
+      )
+    })
     const proc = spawn(isDev ? pathDev : pathProd, [
       '-f',
       isDev ? pathDevSettings : pathProdSettings
@@ -35,16 +59,8 @@ const spawnTor = () =>
     })
   })
 const getOnionAddress = () => {
-  const pathDev = path.join.apply(null, [
-    process.cwd(),
-    'tor/hidden_service/hostname'
-  ])
-  const pathProd = path.join.apply(null, [
-    process.resourcesPath,
-    'tor/hidden_serviceProd/hostname'
-  ])
   var fs = require('fs')
-  const address = fs.readFileSync(isDev ? pathDev : pathProd, 'utf8')
+  const address = fs.readFileSync(`${os.homedir()}/zbay_tor/hostname`, 'utf8')
   return address
 }
 module.exports = { spawnTor, getOnionAddress }
