fc60d7340e931dd9d617b645c556e6bda62cd4b0
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2020-11-03 16:28:00 +0100

    stable, but unfinished version of notifications, users, utils converted to ts

---
+++ src/renderer/store/handlers/notifications.ts
@@ -0,0 +1,36 @@
+import { produce } from "immer";
+import { createAction, handleActions } from "redux-actions";
+import { actionTypes } from "../../../shared/static";
+
+export const initialState = [];
+
+const enqueueSnackbar = createAction(actionTypes.ENQUEUE_SNACKBAR, (n) => ({
+  key: new Date().getTime() + Math.random(),
+  ...n,
+}));
+const removeSnackbar = createAction(actionTypes.REMOVE_SNACKBAR);
+
+export const actions = {
+  enqueueSnackbar,
+  removeSnackbar,
+};
+
+// TODO: [refactoring] rewrite rest of the notifications to use Notifier
+export const reducer = handleActions(
+  {
+    [enqueueSnackbar.toString()]: (state, { payload: notification }) => {
+      console.log("test", notification);
+      return produce(state, (draft) => {
+        draft.push(notification);
+      });
+    },
+    [removeSnackbar.toString()]: (state, { payload: key }) =>
+      produce(state, (draft) => draft.filter((n) => n.key !== key)),
+  },
+  initialState
+);
+
+export default {
+  actions,
+  reducer,
+};
