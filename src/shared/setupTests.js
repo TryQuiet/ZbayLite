00830127f9ba855667790c7b802317ebc325f834
Marek Kowalski <kowalski0123@gmail.com>
2021-02-24 17:13:49 +0100

    Fix configuration for running tests

--- src/shared/setupTests.js
+++ src/shared/setupTests.js
@@ -1,6 +1,7 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import registerRequireContextHook from 'babel-plugin-require-context-hook/register'
+import MemoryStorage from 'redux-persist-memory-storage'
 jest.mock('electron-store-webpack-wrapper')
 jest.mock('./electronStore', () => ({
   get: () => {},
@@ -9,7 +10,7 @@ jest.mock('./electronStore', () => ({
 jest.mock('electron', () => {
   return { ipcRenderer: { on: () => {} } }
 })
-
+jest.mock('redux-persist-electron-storage', () => () => new MemoryStorage())
 global.fetch = jest.fn(() => Promise.resolve())
 registerRequireContextHook()
 process.env.ZBAY_IS_TESTNET = 1
