7ca17c12ec0a782aed233169a2d1ca5a4d28de6a
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2021-09-27 13:39:27 +0200

    settings moda

--- src/renderer/components/widgets/settings/SettingsModal/SettingsModal.js
+++ src/renderer/components/widgets/settings/SettingsModal/SettingsModal.js
@@ -11,6 +11,7 @@ import { Scrollbars } from 'rc-scrollbars'
 import Modal from '../../../ui/Modal/Modal'
 import Tab from '../../../ui/Tab/Tab'
 import AccountSettingsForm from '../../../../containers/widgets/settings/AccountSettingsForm'
+import ShippingSettingsForm from '../../../../containers/widgets/settings/ShippingSettingsForm'
 import Security from '../../../../containers/widgets/settings/Security'
 import Notifications from '../../../../containers/widgets/settings/Notifications'
 import BlockedUsers from '../../../../containers/widgets/settings/BlockedUsers'
@@ -18,6 +19,7 @@ import BuyZcash from '../BuyZcash'
 
 const tabs = {
   account: AccountSettingsForm,
+  shipping: ShippingSettingsForm,
   security: Security,
   notifications: Notifications,
   blockedusers: BlockedUsers,
@@ -131,6 +133,11 @@ export const SettingsModal = ({
               <Tab
                 value='notifications'
                 label='Notifications'
+                classes={{ tabRoot: classes.tab, selected: classes.selected }}
+              />
+              <Tab
+                value='shipping'
+                label='Shipping'
                 classes={{ tabRoot: classes.tab, selected: classes.selected }}
               />
               <Tab
