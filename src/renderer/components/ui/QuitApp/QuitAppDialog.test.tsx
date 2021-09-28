6fb429aa072db45ff15273ae6a0796c2e2399ea6
siepra <wiktor.siepra@gmail.com>
2021-09-28 18:40:43 +0200

    Sample test converted

---
+++ src/renderer/components/ui/QuitApp/QuitAppDialog.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { renderComponent } from '../../../testUtils/renderComponent'
+import { QuitAppDialog } from './QuitAppDialog'
+
+describe('QuitAppDialog', () => {
+  it('renders component', () => {
+    const result = renderComponent(
+      <QuitAppDialog open handleClose={jest.fn()} handleQuit={jest.fn()} />
+    );
+
+    expect(result).toMatchSnapshot()
+  })
+})
