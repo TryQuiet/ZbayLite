1d06cb1f4190a47eb8574f58773af32ebe2334ec
siepra <wiktor.siepra@gmail.com>
2021-09-29 14:28:13 +0200

    Not working version for debugging

--- src/renderer/components/ui/QuitApp/QuitAppDialog.test.tsx
+++ src/renderer/components/ui/QuitApp/QuitAppDialog.test.tsx
@@ -3,12 +3,37 @@ import React from 'react'
 import { renderComponent } from '../../../testUtils/renderComponent'
 import { QuitAppDialog } from './QuitAppDialog'
 
-describe('QuitAppDialog', () => {
-  it('renders component', () => {
-    const result = renderComponent(
-      <QuitAppDialog open handleClose={jest.fn()} handleQuit={jest.fn()} />
-    )
 
-    expect(result).toMatchSnapshot()
+import renderer, { act, ReactTestRenderer } from 'react-test-renderer';
+
+import { MuiThemeProvider } from '@material-ui/core';
+import theme from '../../../theme';
+import ReactDOM from 'react-dom';
+
+describe('QuitAppDialog', () => {
+
+  beforeAll(() => {
+    ReactDOM.createPortal = jest.fn((element, _node) => {
+      return element
+    })
+  })
+
+  it('renders component', () => {
+    let wrapper: ReactTestRenderer
+    act(() => {
+      wrapper = renderer.create(
+        <MuiThemeProvider theme={theme}>
+          <QuitAppDialog open handleClose={jest.fn()} handleQuit={jest.fn()} />
+        </MuiThemeProvider>
+      )
+    })
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+
+    // const result = renderComponent(
+    //   <QuitAppDialog open handleClose={jest.fn()} handleQuit={jest.fn()} />
+    // )
+
+    // expect(result).toMatchSnapshot()
   })
 })
