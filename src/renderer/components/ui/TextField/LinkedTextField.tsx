e2dd5fd8e4c76475fe655f8cdfb84560b3b0a3ec
Wiktor Sieprawski <wiktor.sieprawski@rumblefish.pl>
2021-09-29 18:21:08 +0200

    Refactoring/convert formik linked text fields helper to ts (#383)
    * SWIP
    
    * Corrected types for helper

--- src/renderer/components/ui/TextField/LinkedTextField.tsx
+++ src/renderer/components/ui/TextField/LinkedTextField.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Field } from 'formik'
-import { formikLinkedTextField } from '../helpers'
+import { formikLinkedTextField } from './helpers'
 
 interface LinkedTextFieldProps {
   name: string
