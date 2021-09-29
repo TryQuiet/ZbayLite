16437349c2704903ce647fb021d54a51c71d23f2
Marek Kowalski <kowalski0123@gmail.com>
2021-09-29 18:46:47 +0200

    Use baseElement instead of asFragment()

--- src/renderer/testUtils/renderComponent.tsx
+++ src/renderer/testUtils/renderComponent.tsx
@@ -5,10 +5,10 @@ import { render } from '@testing-library/react'
 
 import theme from '../theme'
 
-export const renderComponent = (ui: ReactElement): DocumentFragment => {
+export const renderComponent = (ui: ReactElement): ReturnType<typeof render> => {
   const Wrapper: FC = ({ children }) => (
     <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
   )
 
-  return render(ui, { wrapper: Wrapper }).asFragment()
+  return render(ui, { wrapper: Wrapper })
 }
