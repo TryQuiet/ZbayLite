ec01a8590429fc38f044d43a4ccb039591cfec7e
siepra <wiktor.siepra@gmail.com>
2021-10-01 13:13:30 +0200

    Containers removed

--- src/renderer/Root.tsx
+++ src/renderer/Root.tsx
@@ -8,22 +8,23 @@ import { HashRouter, Route } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
 import store from './store'
-import Index from './containers/windows/Index'
 import Main from './containers/windows/Main'
-import Vault from './containers/windows/Vault'
 import Loading from './containers/windows/Loading'
-import Notifier from './containers/ui/Notifier'
+import Vault from './containers/windows/Vault'
+import AddModerator from './containers/widgets/channelSettings/AddModerator'
+import ChannelSettingsModal from './containers/widgets/channelSettings/ChannelSettingsModal'
+import CreateChannelModal from './containers/widgets/channels/CreateChannelModal'
+import CreateUsernameModal from './containers/widgets/createUsernameModal/CreateUsername'
 import ErrorModal from './containers/ui/ErrorModal'
-import QuitAppDialog from './containers/ui/QuitAppDialog'
+import FailedUsernameRegister from './containers/ui/FailedUsernameRegister'
+import JoinChannelModal from './containers/widgets/channels/JoinChannelModal'
+import NewMessageModal from './containers/widgets/channels/NewMessageModal'
+import Notifier from './containers/ui/Notifier'
+import OpenExternalLinkModal from './containers/ui/OpenExternalLinkModal'
+import SettingsModal from './containers/widgets/settings/SettingsModal'
 import SecurityModal from './containers/widgets/settings/SecurityModal'
 import UpdateModal from './containers/widgets/update/UpdateModal'
-import CreateChannelModal from './containers/widgets/channels/CreateChannelModal'
-import NewMessageModal from './containers/widgets/channels/NewMessageModal'
-import JoinChannelModal from './containers/widgets/channels/JoinChannelModal'
-import ChannelSettingsModal from './containers/widgets/channelSettings/ChannelSettingsModal'
-import OpenExternalLinkModal from './containers/ui/OpenExternalLinkModal'
-import AddModerator from './containers/widgets/channelSettings/AddModerator'
-import FailedUsernameRegister from './containers/ui/FailedUsernameRegister'
+import QuitAppDialog from './containers/ui/QuitAppDialog'
 import theme from './theme'
 
 export default () => {
@@ -34,23 +35,24 @@ export default () => {
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             <SnackbarProvider maxSnack={3}>
-              <Notifier />
-              <ErrorModal />
-              <QuitAppDialog />
-              <UpdateModal />
-              <SecurityModal />
-              <CssBaseline />
-              <CreateChannelModal />
-              <NewMessageModal />
-              <JoinChannelModal />
-              <ChannelSettingsModal />
               <AddModerator />
-              <OpenExternalLinkModal />
+              <ChannelSettingsModal />
+              <CreateChannelModal />
+              <CreateUsernameModal />
+              <CssBaseline />
+              <ErrorModal />
               <FailedUsernameRegister />
-              <Route path='/vault' exact component={Vault} />
+              <JoinChannelModal />
+              <NewMessageModal />
+              <Notifier />
+              <OpenExternalLinkModal />
+              <SettingsModal />
+              <SecurityModal />
+              <UpdateModal />
+              <QuitAppDialog />
               <Route path='/main' component={Main} />
-              <Route path='/zcashNode' component={Index} />
               <Route path='/loading' component={Loading} />
+              <Route path='/vault' exact component={Vault} />
             </SnackbarProvider>
           </PersistGate>
         </Provider>
