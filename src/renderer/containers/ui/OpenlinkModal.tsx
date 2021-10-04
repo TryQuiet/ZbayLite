8a1c29c91785f690cfca3e0c7cc588268eccac38
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2021-10-04 11:00:37 +0200

    remove react.memo from openLinkModal container

--- src/renderer/containers/ui/OpenlinkModal.tsx
+++ src/renderer/containers/ui/OpenlinkModal.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import OpenlinkModalComponent from '../../components/ui/OpenlinkModal/OpenlinkModal'
 
-export default React.memo(OpenlinkModalComponent)
+export default OpenlinkModalComponent
