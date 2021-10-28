59b234b65bfdcef9935499ad6d8c0ae74ec53302
siepra <wiktor.siepra@gmail.com>
2021-10-28 15:25:33 +0200

    Enable passing props to modals. Move communities logic after typing in username

--- src/renderer/containers/hooks.ts
+++ src/renderer/containers/hooks.ts
@@ -3,16 +3,26 @@ import { modalsSelectors } from '../sagas/modals/modals.selectors'
 import { modalsActions } from '../sagas/modals/modals.slice'
 import { ModalName } from '../sagas/modals/modals.types'
 
-export const useModal = (name: ModalName) => {
+export const useModal = <T>(name: ModalName) => {
   const dispatch = useDispatch()
 
   const open = useSelector(modalsSelectors.open(name))
-  const handleOpen = () => dispatch(modalsActions.openModal(name))
+  const props: T = useSelector(modalsSelectors.props(name))
+
+  const handleOpen = (args?: T) =>
+    dispatch(
+      modalsActions.openModal({
+        name: name,
+        args: args
+      })
+    )
+
   const handleClose = () => dispatch(modalsActions.closeModal(name))
 
   return {
     open,
     handleOpen,
-    handleClose
+    handleClose,
+    ...props
   }
 }
