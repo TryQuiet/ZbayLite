2fe5d18aafc7609f976ec51da6f76af918e0aa80
siepra <wiktor.siepra@gmail.com>
2021-11-02 16:14:14 +0100

    Trying to involve nectar sagas into forntend tests

--- src/renderer/containers/widgets/createCommunity/createCommunity.test.tsx
+++ src/renderer/containers/widgets/createCommunity/createCommunity.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import '@testing-library/jest-dom/extend-expect';
-import { combineReducers, createStore } from '@reduxjs/toolkit'
+import { Socket } from 'socket.io-client'
+import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
+import { applyMiddleware, combineReducers, createStore } from '@reduxjs/toolkit'
+import { fork, take } from 'typed-redux-saga'
+import createSagaMiddleware from 'redux-saga'
+import thunk from 'redux-thunk'
 import { communities, identity, users, errors, messages, publicChannels } from '@zbayapp/nectar'
 import { socketReducer, SocketState } from '../../../sagas/socket/socket.slice'
 import { ModalsInitialState, modalsReducer } from '../../../sagas/modals/modals.slice'
@@ -8,11 +13,12 @@ import { StoreKeys as NectarStoreKeys } from '@zbayapp/nectar/lib/sagas/store.ke
 import { StoreKeys } from '../../../store/store.keys'
 import { renderComponent } from '../../../testUtils/renderComponent'
 import CreateCommunity from './createCommunity'
-import CreateUsernameModal from '../createUsernameModal/CreateUsername';
+import CreateUsernameModal from '../createUsernameModal/CreateUsername'
 import { ModalName } from '../../../sagas/modals/modals.types'
 import { screen } from '@testing-library/dom'
 import { CreateCommunityDictionary } from '../../../components/widgets/performCommunityAction/PerformCommunityAction.dictionary'
-import userEvent from '@testing-library/user-event';
+
+const socket = (jest.fn() as unknown) as Socket
 
 const reducers = {
   [NectarStoreKeys.Communities]: communities.reducer,
@@ -27,35 +33,65 @@ const reducers = {
 
 export const prepareStore = (reducers, mockedState?: { [key in StoreKeys]?: any }) => {
   const combinedReducers = combineReducers(reducers)
-  return createStore(combinedReducers, mockedState)
+  const sagaMiddleware = createSagaMiddleware()
+  const store = createStore(
+    combinedReducers,
+    mockedState,
+    applyMiddleware(...[sagaMiddleware, thunk])
+  )
+  return {
+    store,
+    runSaga: sagaMiddleware.run
+  }
 }
 
-test('User enters community name and is being redirected to username registration after hitting button', async () => {
-  const store = prepareStore(reducers, {
+describe('test', () => {
+  it('User enters community name and is being redirected to username registration after hitting button', async () => {
+    const { store, runSaga } = prepareStore(reducers, {
       [StoreKeys.Socket]: {
-          ...new SocketState(),
-          isConnected: true
+        ...new SocketState(),
+        isConnected: true
       },
       [StoreKeys.Modals]: {
-          ...new ModalsInitialState(),
-          [ModalName.createCommunityModal]: { open: true }
+        ...new ModalsInitialState(),
+        [ModalName.createCommunityModal]: { open: true }
       }
+    })
+
+    renderComponent(
+      <>
+        <CreateCommunity />
+        <CreateUsernameModal />
+      </>,
+      store
+    )
+
+    // Confirm proper modal title is displayed
+    const dictionary = CreateCommunityDictionary()
+    const createCommunityTitle = screen.getByText(dictionary.header)
+    expect(createCommunityTitle).toBeVisible()
+
+    // Enter community name and hit button
+    const createCommunityInput = screen.getByPlaceholderText(dictionary.placeholder)
+    const createCommunityButton = screen.getByText(dictionary.button)
+    userEvent.type(createCommunityInput, 'rockets')
+    userEvent.click(createCommunityButton)
+
+    // Confirm user is being redirected to username registration
+    const createUsernameTitle = await screen.findByText('Register a username')
+    expect(createUsernameTitle).toBeVisible()
+
+    // Enter username and hit button
+    const createUsernameInput = await screen.findByPlaceholderText('Enter a username')
+    const createUsernameButton = await screen.findByText('Register')
+    userEvent.type(createUsernameInput, 'holmes')
+    userEvent.click(createUsernameButton)
+
+    await runSaga(testCreateCommunitySaga).toPromise()
   })
-
-  renderComponent(<><CreateCommunity /><CreateUsernameModal /></>, store)
-
-  // Confirm proper modal title is displayed
-  const dictionary = CreateCommunityDictionary()
-  const createCommunityTitle = screen.getByText(dictionary.header)
-  expect(createCommunityTitle).toBeVisible()
-
-  // Enter community name and hit button
-  const createCommunityInput = screen.getByPlaceholderText(dictionary.placeholder)
-  const createCommunityButton = screen.getByText(dictionary.button)
-  userEvent.type(createCommunityInput, 'rockets')
-  userEvent.click(createCommunityButton)
-
-  // Confirm user is being redirected to username registration
-  const createUsernameTitle = await screen.findByText('Register a username')
-  expect(createUsernameTitle).toBeVisible()
 })
+
+function* testCreateCommunitySaga(): Generator {
+  yield* fork(communities.sagas, socket)
+  yield* take(communities.actions.setCurrentCommunity)
+}
